1f05b1fe8fd4dec14607001d15611203052e6ef5
b0s00dg <b0s00dg@homeoffice.wal-mart.com>
2021-04-19 22:15:30 -0500

    added zsh plugin install script

---
+++ run/after/zsh_plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# download 'zsh-autosuggestions' plugin
+cd ~/.zsh && git clone git@github.com:zsh-users/zsh-autosuggestions.git
+
+# download 'fast-syntax-highlighting' plugin
+cd ~/.zsh && git clone git@github.com:zdharma/fast-syntax-highlighting.git
